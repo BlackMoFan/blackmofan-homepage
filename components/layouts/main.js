fd5186c0962080c1fca71905abbb9e6f74a5a887
BlackMoFan <82863675+BlackMoFan@users.noreply.github.com>
2021-11-18 00:33:40 +0800

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,7 +9,7 @@ const Main = ({children, router}) => {
 		<Box as="main" pb={8}>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>Black Mo Fan - Homepage</title>
+				<title>Black Mo Fan</title>
 			</Head>
 
 			<Navbar path={router.asPath} />
