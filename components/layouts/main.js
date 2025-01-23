928182138ad6e5ea9a657ae5051c54a1bd29d71f
BlackMoFan <rodlester.moreno23@gmail.com>
2025-01-24 04:04:58 +0800

    updated metadata

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,8 +8,13 @@ const Main = ({children, router}) => {
 	return (
 		<Box as="main" pb={8}>
 			<Head>
+				<title>Rod Lester A. Moreno - Portfolio</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>Black Mo Fan</title>
+				<meta name="description" content="Rod Moreno's website portfolio showcasing his projects in software development" />
+				<meta name="keywords" content="rod, rod lester, rod moreno, rod lester moreno, blackmofan" />
+				<meta property="og:title" content="Rod Lester A. Moreno - Portfolio" />
+				<meta property="og:description" content="Rod Moreno's website portfolio showcasing his projects in software development" />
+				<meta property="og:image" content="https://blackmofan.vercel.app/images/blackmofan.jpg" />
 			</Head>
 
 			<Navbar path={router.asPath} />
