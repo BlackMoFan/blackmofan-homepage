d3c01dec237bbb41eab812bd0e8c51e8092919cc
BlackMoFan <rodlester.moreno23@gmail.com>
2025-03-24 01:32:36 +0800

    added hire page and email service using resend

--- pages/contact.js
+++ pages/contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
         const data = Object.fromEntries(formData.entries())
 
         try {
-            const response = await fetch('http://localhost:5000/send-email', {
+            const response = await fetch('/api/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
